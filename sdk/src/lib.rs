7938f75e95f9055c8ed7705ba00d272aab2c8c8a
ivan770 <ivan@ivan770.me>
2025-06-04 17:12:42 +0000

    Make AssertionDefinition public

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -123,7 +123,7 @@ pub mod validation_status;
 pub use assertions::Relationship;
 #[cfg(feature = "v1_api")]
 pub use asset_io::{CAIRead, CAIReadWrite};
-pub use builder::{Builder, ManifestDefinition};
+pub use builder::{AssertionDefinition, Builder, ManifestDefinition};
 pub use callback_signer::{CallbackFunc, CallbackSigner};
 pub use claim_generator_info::ClaimGeneratorInfo;
 // pub use dynamic_assertion::{
